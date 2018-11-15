00735580d81d900484d7ae78657513cbf76dda54
Imogen Hardy <imogen.hardy@madgex.com>
2018-11-15 14:59:28 +0000

    Refactor to stop creating a new blank SVG every time

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-let emptySvg;
-
-function getComparisonSvg() {
-  emptySvg = window.document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  window.document.body.appendChild(emptySvg);
-  return getComputedStyle(emptySvg);
-}
+let emptyStyles;
 
 function setStyle(element) {
-  const blankSvgStyles = getComparisonSvg();
   const elementStyleComputed = getComputedStyle(element);
   let computedStyleStr = '';
   for (let i = 0; i < elementStyleComputed.length; i++) {
     const key = elementStyleComputed.item(i);
     const value = elementStyleComputed.getPropertyValue(key);
-    if (value !== blankSvgStyles.getPropertyValue(key)) {
+    if (value !== emptyStyles.getPropertyValue(key)) {
       computedStyleStr += `${key}:${value};`;
     }
   }
   element.setAttribute('style', computedStyleStr);
-  emptySvg.remove();
 }
 
 export default {
   bundle(svg) {
+    const emptySvg = window.document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'svg'
+    );
+    window.document.body.appendChild(emptySvg);
+    emptyStyles = getComputedStyle(emptySvg);
     const treeWalker = document.createTreeWalker(
       svg,
       NodeFilter.SHOW_ELEMENT,
       {
         acceptNode: () => NodeFilter.FILTER_ACCEPT,
       },
-      false,
+      false
     );
 
     do {
       setStyle(treeWalker.currentNode);
     } while (treeWalker.nextNode());
+    emptySvg.remove();
   },
 };
